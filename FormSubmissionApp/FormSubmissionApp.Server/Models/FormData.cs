89d9bff059cbfb0a7cb6bd2260e263150ecf6ec8
Azwad Islam <azwad.sachcha@gmail.com>
2025-04-28 00:40:59 +0600

    commit

--- FormSubmissionApp/FormSubmissionApp.Server/Models/FormData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FormSubmissionApp.Models
-{
-    public class FormData
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-    }
-}
